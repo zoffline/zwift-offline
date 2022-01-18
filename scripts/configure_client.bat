e8e58ab25f8b72758f311cb68cbaf3149bd85047
oldnapalm <38410858+oldnapalm@users.noreply.github.com>
2022-01-18 11:12:35 -0300

    Update configure_client.bat

--- scripts/configure_client.bat
+++ scripts/configure_client.bat
@@ -16,7 +16,7 @@ ECHO 127.0.0.1 us-or-rly101.zwift.com secure.zwift.com cdn.zwift.com launcher.zw
 ECHO.
 
 FOR /F "tokens=4-5 delims=. " %%I IN ('ver') DO SET VERSION=%%I
-certutil.exe -store Root | FIND /C /I "6e6a20aafc42860d24a054580f7191e531542a29" >nul 2>&1
+certutil.exe -store Root | FIND /C /I "54f7f293407370a07679885767e5bd599458e471" >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     ECHO Importing certificate
     IF %VERSION% == 10 ( ECHO.|certutil.exe -importpfx Root ..\ssl\cert-zwift-com.p12
@@ -37,7 +37,7 @@ SET CACERT=%FOLDER%\data\cacert.pem
 IF NOT EXIST %CACERT% GOTO:NOT_FOUND
 ECHO %FOLDER%>%ZWIFT%
 :FOUND
->nul 2>&1 FIND /C "MIIEMzCCAxugAwIBAgIUbmogqvxChg0koFRYD3GR5TFUKikwDQYJKoZIhvcNAQEL" %CACERT%
+>nul 2>&1 FIND /C "MIIEQTCCAymgAwIBAgIUVPfyk0BzcKB2eYhXZ+W9WZRY5HEwDQYJKoZIhvcNAQEL" %CACERT%
 IF %ERRORLEVEL% NEQ 0 (
     ECHO Adding certificate to cacert.pem
     ECHO.>>%CACERT%
