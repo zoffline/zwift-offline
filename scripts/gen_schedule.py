3622f650e4d2901d0bd558985cecbf2c3f0f7237
oldnapalm <38410858+oldnapalm@users.noreply.github.com>
2023-04-05 22:45:14 -0300

    Update map schedule

--- scripts/gen_schedule.py
+++ scripts/gen_schedule.py
@@ -9,14 +9,13 @@ import xml.etree.ElementTree as ET
 
 from xml.dom import minidom
 
-# Make london and makuri islands more likely
-MAPS = [ 'FRANCE' ] + [ 'INNSBRUCK' ] + [ 'LONDON' ] * 2 + [ 'MAKURIISLANDS' ] * 2 + [ 'NEWYORK' ] + [ 'RICHMOND' ] + [ 'SCOTLAND' ] + [ 'YORKSHIRE' ]
+MAPS = [ 'FRANCE' ] + [ 'INNSBRUCK' ] + [ 'LONDON' ] + [ 'MAKURIISLANDS' ] + [ 'RICHMOND' ] + [ 'SCOTLAND' ]
 
 
 dom = minidom.parseString('<MapSchedule><appointments></appointments><VERSION>1</VERSION></MapSchedule>')
 appts = dom.getElementsByTagName('appointments')[0]
 
-now = datetime.datetime.utcnow()
+now = datetime.datetime.utcnow().replace(day=1)
 prev_map = None
 for i in range(0, 500):
     map_choice = random.choice(MAPS)
@@ -28,10 +27,6 @@ for i in range(0, 500):
     appt.setAttribute('start', now.strftime("%Y-%m-%dT00:01-04"))
 
     appts.appendChild(appt)
-    # Shorter duration for UCI course maps
-    if map_choice in [ 'INNSBRUCK', 'RICHMOND', 'SCOTLAND', 'YORKSHIRE' ]:
-        now += datetime.timedelta(days=random.randint(1,2))
-    else:
-        now += datetime.timedelta(days=random.randint(1,5))
+    now += datetime.timedelta(days=2)
 
 sys.stdout.write(dom.toprettyxml())
