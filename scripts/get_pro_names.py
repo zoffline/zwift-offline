4c5301143ac97b0b2a5b7bf6cd186086c8c9f86d
oldnapalm <38410858+oldnapalm@users.noreply.github.com>
2023-04-11 12:21:20 -0300

    Update get_pro_names.py

--- scripts/get_pro_names.py
+++ scripts/get_pro_names.py
@@ -16,9 +16,6 @@ def get_pros(url, male):
     site = urllib.request.urlopen(req).read()
     soup = BeautifulSoup(site)
 
-    def has_class(tag):
-        return tag.has_attr('class')
-
     for td in soup.find_all('td'):
         if td.span and td.contents[0]:
             if "flag" in repr(td.contents[0]):
@@ -33,7 +30,7 @@ def get_pros(url, male):
     return data
 
 total_data = {}
-total_data['riders'] = (get_pros(male_url, "True")) + (get_pros(female_url, "False"))
+total_data['riders'] = get_pros(male_url, True) + get_pros(female_url, False)
 total_data['body_types'] = [16, 48, 80, 272, 304, 336, 528, 560, 592]
 total_data['hair_types'] = [25953412, 175379869, 398510584, 659452569, 838618949, 924073005, 1022111028, 1262230565, 1305767757, 1569595897, 1626212425, 1985754517, 2234835005, 2507058825, 3092564365, 3200039653, 3296520581, 3351295312, 3536770137, 4021222889, 4179410997, 4294226781]
 total_data['facial_hair_types'] = [248681634, 398510584, 867351826, 1947387842, 2173853954, 3169994930, 4131541011, 4216468066]
