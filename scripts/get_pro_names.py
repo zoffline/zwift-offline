5f9ae395d9cd796a20295978521f062f9a122f9e
Daktak <daktak@domo.ddns.net>
2023-04-11 18:49:51 +1000

    pro_name getter and file

---
+++ scripts/get_pro_names.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+from bs4 import BeautifulSoup
+import urllib.request
+from phonenumbers.phonenumberutil import country_code_for_region
+import json
+
+male_url = "https://www.procyclingstats.com/rankings/me/individual"
+female_url = "https://www.procyclingstats.com/rankings/we/individual"
+
+def get_pros(url):
+    data = []
+    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    site = urllib.request.urlopen(req).read()
+    soup = BeautifulSoup(site)
+
+    def has_class(tag):
+        return tag.has_attr('class')
+
+    for td in soup.find_all('td'):
+        if td.span and td.contents[0]:
+            if "flag" in repr(td.contents[0]):
+                tmp = {}
+                code = td.span.get_attribute_list("class")[1]
+                tmp['country_code'] = (country_code_for_region(code.upper()))
+                if td.a:
+                    tmp['first_name'] = (td.a.contents[1].strip())
+                    tmp['last_name'] = (td.a.span.contents[0])
+                    data.append(tmp)
+    return data
+
+total_data = {}
+total_data['male_riders'] = get_pros(male_url)
+total_data['female_riders'] = get_pros(female_url)
+
+with open('pro_bots.json','w') as outfile:
+    json.dump(total_data, outfile, indent=2)
