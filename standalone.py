e91288967a1588bf7a98308c96837844f7430bad
oldnapalm <38410858+oldnapalm@users.noreply.github.com>
2023-04-27 10:18:00 -0300

    Update standalone.py

--- standalone.py
+++ standalone.py
@@ -23,7 +23,6 @@ import zwift_offline as zo
 import udp_node_msgs_pb2
 import tcp_node_msgs_pb2
 import profile_pb2
-import numpy as np
 
 if getattr(sys, 'frozen', False):
     # If we're running as a pyinstaller bundle
@@ -532,7 +531,8 @@ def load_bots():
     with open('%s/bot.txt' % SCRIPT_DIR) as f:
         data = json.load(f)
     i = 1
-    loop_riders = np.random.permutation(data['riders'])
+    random.shuffle(data['riders'])
+    loop_riders = data['riders'].copy()
     for name in os.listdir(STORAGE_DIR):
         path = '%s/%s/ghosts' % (STORAGE_DIR, name)
         if os.path.isdir(path):
@@ -551,7 +551,7 @@ def load_bots():
                         p.body_type = random.choice(data['body_types'])
                         p.hair_type = random.choice(data['hair_types'])
                         if not loop_riders:
-                          loop_riders = np.random.permutation(data['riders'])
+                            loop_riders = data['riders'].copy()
                         rider = loop_riders.pop()
                         p.is_male = rider['is_male']
                         if p.is_male:
