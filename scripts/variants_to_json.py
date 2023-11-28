5e9c0613d38eea14b89a50f8fb3dd1716cfe9014
oldnapalm <38410858+oldnapalm@users.noreply.github.com>
2023-11-28 00:01:44 -0300

    Remove unused features

--- scripts/variants_to_json.py
+++ scripts/variants_to_json.py
@@ -8,13 +8,14 @@ with open("variant", "rb") as f:
     variants = variants_pb2.FeatureResponse()
     variants.ParseFromString(f.read())
 
+keep = ['zwift_launcher_']
+
 with open("../variants.txt") as f:
-    vs = json.load(f)['variants']
+    vs = [d for d in json.load(f)['variants'] if any(d['name'].startswith(s) for s in keep)]
 
 for v in MessageToDict(variants)['variants']:
     if 'fields' in v['values']:
         v['values']['fields'] = dict(sorted(v['values']['fields'].items()))
-    vs[:] = [d for d in vs if d.get('name') != v['name']]
     vs.append(v)
 
 with open("../variants.txt", "w") as f:
