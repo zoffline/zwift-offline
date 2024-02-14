b1e298248df000fd535a9d4066fbce738bce8ba2
oldnapalm <38410858+oldnapalm@users.noreply.github.com>
2024-02-14 16:49:13 -0300

    Update bot editor

--- scripts/bot_editor.py
+++ scripts/bot_editor.py
@@ -2,7 +2,8 @@
 #
 # Route ID can be found in http://cdn.zwift.com/gameassets/GameDictionary.xml (signature)
 #
-# To find new start line values type /startline in chat, the values will be printed in console
+# Type .position in chat to find start road and roadTime, the values will be printed in console
+# Choose a position where the bot passes both times at the same speed to have a smooth loop
 
 import os
 import sys
@@ -25,11 +26,6 @@ def is_forward(state):
 def get_course(state):
     return (state.f19 & 0xff0000) >> 16
 
-def boolean(s):
-    if s.lower() in ['true', 'yes', '1']: return True
-    if s.lower() in ['false', 'no', '0']: return False
-    return None
-
 def delete(s, i):
     print('course %s road %s isForward %s roadTime %s' % (get_course(s[i]), road_id(s[i]), is_forward(s[i]), s[i].roadTime))
     del s[i]
@@ -50,8 +46,7 @@ if file_exists(PROFILE_FILE):
     p.last_name = input("Last name: ")
     for a in p.public_attributes:
         #0x69520F20=1766985504 - crc32 of "PACE PARTNER - ROUTE"
-        #TODO: -1021012238: figure out
-        if a.id == 1766985504 or a.id == -1021012238:  #-1021012238 == 3273955058
+        if a.id == 1766985504:
             a.number_value = int(input("Route ID: "))
     with open(PROFILE_FILE, 'wb') as f:
         f.write(p.SerializeToString())
@@ -61,37 +56,27 @@ if file_exists(ROUTE_FILE):
     g = udp_node_msgs_pb2.Ghost()
     with open(ROUTE_FILE, 'rb') as f:
         g.ParseFromString(f.read())
-    START_LINES_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'bot_start_lines.csv')
-    if file_exists(START_LINES_FILE) and len(g.states) > 0:
-        f = open(START_LINES_FILE, 'r')
-        sl = [tuple(line) for line in csv.reader(f)]
-        f.close()
-        rt = [t for t in sl if t[0] == str(get_course(g.states[0])) and t[1] == str(road_id(g.states[0])) and (boolean(t[2]) == is_forward(g.states[0]) or not t[2])]
-        if rt:
-            start_road = int(rt[0][3])
-            start_rt = int(rt[0][4])
-            print('Start line: course %s road %s isForward %s roadTime %s\n' % (get_course(g.states[0]), start_road, is_forward(g.states[0]), start_rt))
-            print('Deleted records:\n')
-            try:
-                while road_id(g.states[0]) != start_road:
-                    delete(g.states, 0)
-                while road_id(g.states[-1]) != start_road:
-                    delete(g.states, -1)
-                if is_forward(g.states[0]):
-                    while g.states[0].roadTime < start_rt or abs(g.states[0].roadTime - start_rt) > 500000:
-                        delete(g.states, 0)
-                    while g.states[-1].roadTime > start_rt or abs(g.states[-1].roadTime - start_rt) > 500000:
-                        delete(g.states, -1)
-                else:
-                    while g.states[0].roadTime > start_rt or abs(g.states[0].roadTime - start_rt) > 500000:
-                        delete(g.states, 0)
-                    while g.states[-1].roadTime < start_rt or abs(g.states[-1].roadTime - start_rt) > 500000:
-                        delete(g.states, -1)
-            except IndexError:
-                pass
-            with open(ROUTE_FILE, 'wb') as f:
-                f.write(g.SerializeToString())
+    start_road = int(input("Start road: "))
+    start_rt = int(input("Start roadTime: "))
+    print('Deleted records:\n')
+    try:
+        while road_id(g.states[0]) != start_road:
+            delete(g.states, 0)
+        while road_id(g.states[-1]) != start_road:
+            delete(g.states, -1)
+        if is_forward(g.states[0]):
+            while g.states[0].roadTime < start_rt or abs(g.states[0].roadTime - start_rt) > 500000:
+                delete(g.states, 0)
+            while g.states[-1].roadTime > start_rt or abs(g.states[-1].roadTime - start_rt) > 500000:
+                delete(g.states, -1)
         else:
-            print('Start line not found (course %s road %s isForward %s)' % (get_course(g.states[0]), road_id(g.states[0]), is_forward(g.states[0])))
+            while g.states[0].roadTime > start_rt or abs(g.states[0].roadTime - start_rt) > 500000:
+                delete(g.states, 0)
+            while g.states[-1].roadTime < start_rt or abs(g.states[-1].roadTime - start_rt) > 500000:
+                delete(g.states, -1)
+    except IndexError:
+        pass
+    with open(ROUTE_FILE, 'wb') as f:
+        f.write(g.SerializeToString())
 
 print('\nDone')
