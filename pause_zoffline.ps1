302f086b9f20f8284a51239fc48d0fbcf3ad4351
oldnapalm <38410858+oldnapalm@users.noreply.github.com>
2025-03-27 14:44:22 -0300

    Move to scripts, merge bat and ps1

--- pause_zoffline.ps1
+++
@@ -1,5 +0,0 @@
-$hostsPath = "$env:windir\System32\drivers\etc\hosts"
-$hosts = get-content $hostsPath
-$hosts | Foreach {if ($_ -match '^\s*([^#].*?\d{1,3}.*?zwift.*)') 
-                  {"# " + $matches[1]} else {$_}} |
-         Out-File $hostsPath -enc ascii
