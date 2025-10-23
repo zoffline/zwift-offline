288a5a31a901f1a21e3fa441117d0e7f6d9093cf
oldnapalm <38410858+oldnapalm@users.noreply.github.com>
2025-10-23 15:12:20 -0300

    Update fake_dns.py

--- fake_dns.py
+++ fake_dns.py
@@ -1,48 +1,31 @@
+import dns.message
+import dns.rdata
 import dns.resolver
 import socketserver
 import os
-
-class DNSQuery:
-    def __init__(self, data):
-        self.data = data
-        self.domain = ''
-        t = (data[2] >> 3) & 15
-        if t == 0:
-            i = 12
-            l = data[i]
-            while l != 0:
-                self.domain += data[i + 1:i + l + 1].decode('utf-8') + '.'
-                i += l + 1
-                l = data[i]
-
-    def response(self):
-        packet = b''
-        if self.domain:
-            name = self.domain
-            namemap = DNSServer.namemap
-            if namemap.__contains__(name):
-                ip = namemap[name]
-            else:
-                answer = DNSServer.resolver.cache.data.get((name, 1, 1))
-                if not answer:
-                    answer = DNSServer.resolver.resolve(name)
-                    DNSServer.resolver.cache.put((name, 1, 1), answer)
-                ip = answer[0].to_text()
-            packet += self.data[:2] + b'\x81\x80'
-            packet += self.data[4:6] + self.data[4:6] + b'\x00\x00\x00\x00'
-            packet += self.data[12:]
-            packet += b'\xc0\x0c'
-            packet += b'\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04'
-            packet += bytes(map(int, ip.split('.')))
-        return packet
 
 class DNSUDPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         data = self.request[0]
         socket = self.request[1]
         try:
-            query = DNSQuery(data)
-            socket.sendto(query.response(), self.client_address)
+            query = dns.message.from_wire(data)
+            response = dns.message.make_response(query)
+            for question in query.question:
+                name = question.name.to_text()
+                if name in DNSServer.namemap and question.rdtype == 1:
+                    ip = DNSServer.namemap[name]
+                else:
+                    answer = DNSServer.resolver.cache.data.get((name, 1, 1))
+                    if not answer:
+                        answer = DNSServer.resolver.resolve(name)
+                        DNSServer.resolver.cache.put((name, 1, 1), answer)
+                    ip = answer[0].to_text()
+                rdata = dns.rdata.from_text(1, 1, ip)
+                rrset = dns.rrset.RRset(question.name, 1, 1)
+                rrset.add(rdata)
+                response.answer.append(rrset)
+            socket.sendto(response.to_wire(), self.client_address)
         except Exception as e:
             print('fake_dns: %s' % repr(e))
 
